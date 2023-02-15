ed7e204deb819ce4155089d964b6f9f2c0fda922
SebastianZimmeck <szimmeck@wesleyan.edu>
2023-02-15 11:12:18 -0500

    Add password hashing example script

---
+++ password_hashing_demo.php
@@ -0,0 +1,15 @@
+<?php
+// Demo for hashing a password.
+
+$password = "TestPassword%12";
+
+// Using PHP's built-in password hashing function (https://www.php.net/manual/en/function.password-hash.php).
+// The first parameter is the password in plaintext and the second parameter is the hashing algorithm.
+// PASSWORD_DEFAULT uses the bcrypt algorithm.
+$hashed_password = password_hash($password, PASSWORD_DEFAULT);
+			
+echo($hashed_password)
+
+// For verifying whether a password a user enters upon login is identical to a hashed password, PHP provides 
+// the built-in password_verify($password, $hashed_password) function (https://www.php.net/manual/en/function.password-verify.php).
+?>
