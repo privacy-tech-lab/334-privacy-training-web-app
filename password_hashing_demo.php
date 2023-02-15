553802ca633a555b4161d3d4047c95888968e905
SebastianZimmeck <szimmeck@wesleyan.edu>
2023-02-15 16:43:51 -0500

    Clarify password hashing example script

--- password_hashing_demo.php
+++ password_hashing_demo.php
@@ -6,8 +6,10 @@ $password = "TestPassword%12";
 // Using PHP's built-in password hashing function (https://www.php.net/manual/en/function.password-hash.php).
 // The first parameter is the password in plaintext and the second parameter is the hashing algorithm.
 // PASSWORD_DEFAULT uses the bcrypt algorithm.
+// Note that password_hash() automatically generates a salt. Thus, the hardcoded password is different
+// every time it is hashed.
 $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-			
+
 echo($hashed_password)
 
 // For verifying whether a password a user enters upon login is identical to a hashed password, PHP provides 
